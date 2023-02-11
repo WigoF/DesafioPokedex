67dee92074988eafe84a5d8e01cb352a31bb77b6
Wigof <wigoferreira.055@gmail.com>
2023-02-11 11:08:53 -0300

    component listpokemon

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+const pokemon = defineProps(['name','baseUrlSvg'])
+
+
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card p-4 mb-3">
+            
+            <img 
+            :src="baseUrlSvg"
+            height="90"
+            />
+            
+            <strong class="text-center">{{ name }}</strong>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,65 @@
 <script setup>
+import{onMounted, reactive, ref} from 'vue'
+import ListPokemons from '../components/ListPokemons.vue';
+
+
+let baseUrlSvg = ref('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/');
+let allPokemons = reactive(ref());
+
+onMounted(()=>{
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+  .then(res => res.json())
+  .then(res => allPokemons.value = res.results)
+})
 
 </script>
 
 <template>
   <main>
     <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <div class="card mt-4 ms-5" style="width: 18rem;">
+      <div class="col-sm-12 col-md-6">
+        <div class="card mt-4 ms-5" style="width: 18rem;">
+              
+              
               <img src="../assets/pokeball.png" class="card-img-top" alt="...">
+
+
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
-            </div>
-          </div>
+
         </div>
+      </div>
+    
+
+        <div class="col-sm-12 col-md-6 ">
+          <div class="card mt-4 ms-5 me-5">
+            <div class="card-body row">
+              
+              
+              
+              <div class="mb-3">
+                <input 
+                v-model="searchPokemonField"
+                type="text" 
+                class="form-control"  
+                placeholder="Pesquisar..."/>
+              </div>
+                        
+              
+                <ListPokemons
+                v-for="pokemon in allPokemons" 
+                :key='pokemon.name'
+                :name='pokemon.name'
+                :baseUrlSvg="baseUrlSvg + pokemon.url.split('/')[6] +'.svg'"/>              
+              
+            
+            
+            
+          </div>  
+        </div>
+      </div>
+  </div>  
   </main>
 </template>
