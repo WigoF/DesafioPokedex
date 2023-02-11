949e84a65431b5b2e9cf1d12e33a72c7375f8c68
Wigof <wigoferreira.055@gmail.com>
2023-02-11 10:04:23 -0300

    header creation and add boostrap

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
- 
+  <div>
+    <header>
+      <nav class="navbar bg-danger ">
+        <div class="container d-flex justify-content-center">
+          <router-link class="navbar-brand" to="/">
+            <img src="./assets/pokeball.png" alt="" width="60" height="60">
+          </router-link>
+        </div>
+      </nav>  
+
+    </header>
+    
+  <div class="main">
+    <router-view></router-view>
+  </div>
+
+  </div>
 </template>
 
 <style scoped>
